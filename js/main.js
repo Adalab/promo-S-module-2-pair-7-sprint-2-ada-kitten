18c554ba9e53d9f73adff250e7123ba157939608
VanesaGR <vanesaglezrguez@gmail.com>
2023-02-16 11:15:07 +0100

    add new kitten object

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const buttonCancelForm = document.querySelector('.js-btn-cancel');
 const inputDesc = document.querySelector('.js-input-desc');
 const inputPhoto = document.querySelector('.js-input-photo');
 const inputName = document.querySelector('.js-input-name');
+const inputRace = document.querySelector('.js-input-race');
 const linkNewFormElememt = document.querySelector('.js-button-new-form');
 const labelMessageError = document.querySelector('.js-label-error');
 const input_search_desc = document.querySelector('.js_in_search_desc');
@@ -79,20 +80,38 @@ function handleClickNewCatForm(event) {
         hideNewCatForm();
     }
 }
-//Adicionar nuevo gatito
+// Adicionar nuevo gatito
+
 function addNewKitten(event) {
     event.preventDefault();
     const valueDesc = inputDesc.value;
     const valuePhoto = inputPhoto.value;
     const valueName = inputName.value;
-    if (valueDesc === "" && valuePhoto === "" && valueName === "") {
-        labelMessageError.innerHTML = "¡Uy! parece que has olvidado algo";
-    } else {
-        if (valueDesc !== "" && valuePhoto !== "" && valueName !== "") {
-            labelMessageError.innerHTML = "";
-        }
-    }
+    const valueRace = inputRace.value;
+    // if (valueDesc === "" && valuePhoto === "" && valueName === "") {
+    //     labelMessageError.innerHTML = "¡Uy! parece que has olvidado algo";
+    // } else {
+    //     if (valueDesc !== "" && valuePhoto !== "" && valueName !== "") {
+    //         labelMessageError.innerHTML = "";
+    //     }
+    // }
+    const newKittenDataObject = {
+        name:valueName,
+        desc:valueDesc,
+        image:valuePhoto,
+        race:valueRace,     
+    };
+        kittenDataList.push(newKittenDataObject);  
+        renderKittenList(kittenDataList);
+        //resetear input
+        valueDesc = "";
+        valuePhoto = "";
+        valueName = "";
+        valueRace = "";
+        labelMesageError.innerHTML = 'Mola! Un nuevo gatito en Adalab!'; 
 }
+
+
 //Cancelar la búsqueda de un gatito
 function cancelNewKitten(event) {
     event.preventDefault();
@@ -114,7 +133,7 @@ function filterKitten(event) {
     }
 }
 
-//Mostrar el litado de gatitos en ell HTML
+//Mostrar el litado de gatitos en el HTML
 renderKittenList(kittenDataList);
 
 //Eventos
